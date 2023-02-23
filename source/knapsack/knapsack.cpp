a2915fb588b7b199a20736d9162bf1898318b666
Felipi Lima Matozinho <matozinhof.academico@gmail.com>
2023-02-23 16:27:06 -0300

    docs(complexity): add asymptotic complexity to the algorithms
    Signed-off-by: Felipi Lima Matozinho <matozinhof.academico@gmail.com>

--- source/knapsack/knapsack.cpp
+++ source/knapsack/knapsack.cpp
@@ -39,40 +39,42 @@ std::tuple<uint32_t, uint32_t> Knapsack::recursive(uint32_t n, uint32_t w,
   return maxBenefit == benefit1 ? std::make_tuple(weight1, benefit1)
                                 : std::make_tuple(weight2, benefit2);
 }
+// clang-format off
+Knapsack* Knapsack::iterative() {                           // O(n * 2^n)
+  uint32_t n = weights.size();                              // 1
 
-Knapsack* Knapsack::iterative() {
-  uint32_t n = weights.size();
+  uint32_t bestValue = 0;                                   // 1   
+  uint32_t bestWeight = 0;                                  // 1
 
-  uint32_t bestValue = 0;
-  uint32_t bestWeight = 0;
+  for (uint32_t i = 0; i < std::pow(2, n); i++) {           // 2^n    
+    uint32_t currentWeight = 0;                             // 2^n
+    uint32_t currentValue = 0;                              // 2^n
 
-  for (uint32_t i = 0; i < std::pow(2, n); i++) {
-    uint32_t currentWeight = 0;
-    uint32_t currentValue = 0;
-
-    for (uint32_t j = 0; j < n; j++) {
-      if (i & (1 << j)) {
-        currentWeight += this->weights[j];
-        currentValue += this->benefits[j];
+    for (uint32_t j = 0; j < n; j++) {                      // 2^n * n
+      if (i & (1 << j)) {                                   // 2^n * n + 3
+        currentWeight += this->weights[j];                  // 2^n * n + 3
+        currentValue += this->benefits[j];                  // 2^n * n + 3
       }
     }
 
-    if ((currentValue > bestValue) && (currentWeight <= this->capacity)) {
-      bestValue = currentValue;
-      bestWeight = currentWeight;
+    if ((currentValue > bestValue) && (currentWeight <= this->capacity)) {      // 2^n * n + 4
+      bestValue = currentValue;                                                 // 2^n * n
+      bestWeight = currentWeight;                                               // 2^n * n
     }
   }
 
-  this->total_benefit = bestValue;
-  this->total_weight = bestWeight;
+  this->total_benefit = bestValue;                                             // 1
+  this->total_weight = bestWeight;                                             // 1
 
   return this;
 }
+// clang-format on
 
 Knapsack* Knapsack::brute_force() {
   /// uncomment it for recursive solution
   // auto [totalWeight, totalBenefit]
-  //     = this->recursive(this->benefits.size(), this->capacity, this->weights, this->benefits, 0, 0);
+  //     = this->recursive(this->benefits.size(), this->capacity, this->weights, this->benefits, 0,
+  //     0);
 
   // this->total_benefit = totalBenefit;
   // this->total_weight = totalWeight;
@@ -82,39 +84,49 @@ Knapsack* Knapsack::brute_force() {
   return this;
 }
 
-Knapsack* Knapsack::greedy() {
-  uint32_t w = this->capacity;
-  std::vector<uint32_t> W(this->weights);
-  std::vector<uint32_t> V(this->benefits);
-  std::vector<uint32_t> x(this->weights.size(), 0);
+// clang-format off
+Knapsack* Knapsack::greedy() {                                // O(n²+n)    
+  uint32_t w = this->capacity;                                // 1
+  std::vector<uint32_t> W(this->weights);                     // n
+  std::vector<uint32_t> V(this->benefits);                    // n
 
-  // v[i] = V[i] / W[i]
-  std::vector<float_t> v(this->benefits.size(), 0);
-  std::transform(V.begin(), V.end(), W.begin(), v.begin(), std::divides<float>());
+  // v = V / W
+  std::vector<float_t> v(this->benefits.size(), 0);           // n+1
+  std::transform(                                             // 4
+    V.begin(),                                                
+    V.end(),                                                                  
+    W.begin(),                                                
+    v.begin(),                                                
+    std::divides<float>()                                     // n
+  );                                                         
 
-  this->total_weight = 0;
-  uint32_t* P = &this->total_weight;
-  this->total_benefit = 0;
+  this->total_weight = 0;                                     // 1
+  uint32_t* P = &this->total_weight;                          // 2
+  this->total_benefit = 0;                                    // 1
 
-  while (*P < w && v.size() > 0) {
-    int idx = std::distance(
-        v.begin(), std::max_element(v.begin(), v.end()));  // get the index of the max element
+  while (*P < w && v.size() > 0) {                            // n
+    /* get the index of the max element */
+    int idx = std::distance(                                  // (n²+n)/2
+      v.begin(),
+      std::max_element(                                       // (n²+n)/2
+        v.begin(),
+        v.end()
+      )
+    );
 
-    int32_t a = std::min(W[idx], w - *P);
+    int32_t a = std::min(W[idx], w - *P);                     // 5n
 
-    if (a == W[idx]) {
-      x[idx] = 1;  // TODO: validate how to get the item idx
-      *P += a;
-      this->total_benefit += V[idx];
+    if (a == W[idx]) {                                        // 2n
+      *P += a;                                                // 2n
+      this->total_benefit += V[idx];                          // 2n
     }
 
     // remove o elemento escolhido
-    v.erase(v.begin() + idx);
-    V.erase(V.begin() + idx);
-    W.erase(W.begin() + idx);
+    v.erase(v.begin() + idx);                                 // 3n
+    V.erase(V.begin() + idx);                                 // 3n
+    W.erase(W.begin() + idx);                                 // 3n
   }
-
-  this->selected_items = x;
 
   return this;
 }
+// clang-format off
